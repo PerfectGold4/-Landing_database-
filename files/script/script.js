efffbfca07a7bb69d00e0f01281b50988197abed
rabotnik-da-da-ya <ilushavolkov@mail.ru>
2022-07-20 18:01:24 +0300

    01

--- files/script/script.js
+++ files/script/script.js
@@ -1,59 +1,5 @@
 $(function(){
 
-			/*$.getJSON('script/json.php', function(data) { 
-				htmlstr = '<ul id="slidewrapper">';          
-  				for (var i=0; i<data.length; i++) {
-  					htmlstr += '<li class="slide">';
-  					htmlstr += '<img src="' + data[i] + '" alt="3" class="slide-img">';
-  					htmlstr += '</li>';
-  				}
-  				htmlstr += '</ul>';
-  				$('.viewport').append(htmlstr);
-			});*/
-
-			function nextSlide() {
-			    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-			        $('#slidewrapper').css('transform', 'translate(0, 0)');
-			        slideNow = 1;
-			    } else {
-			        translateWidth = -$('#viewport').width() * (slideNow);
-			        $('#slidewrapper').css({
-			            'transform': 'translate(' + translateWidth + 'px, 0)',
-			            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-			            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-			        });
-			        slideNow++;
-			    }
-			}
-
-			function prevSlide() {
-			    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
-			        translateWidth = -$('#viewport').width() * (slideCount - 1);
-			        $('#slidewrapper').css({
-			            'transform': 'translate(' + translateWidth + 'px, 0)',
-			            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-			            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-			        });
-			        slideNow = slideCount;
-			    } else {
-			        translateWidth = -$('#viewport').width() * (slideNow - 2);
-			        $('#slidewrapper').css({
-			            'transform': 'translate(' + translateWidth + 'px, 0)',
-			            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-			            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-			        });
-			        slideNow--;
-			    }
-			}
-
-			var slideNow = 1;
-			var slideCount = $('#slidewrapper').children().length;
-			var translateWidth = 0;
-
-			$("#prev-btn").click (function() {prevSlide();});
-			$("#next-btn").click (function() {nextSlide();});
-
-
 
 	window.onbeforeunload = function () {
 	  window.scrollTo(0, 0);
