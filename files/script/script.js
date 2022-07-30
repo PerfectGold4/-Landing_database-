f526cfb85ec91396bea175fe405bf32b1ab1b9b1
rabotnik-da-da-ya <ilushavolkov@mail.ru>
2022-07-30 12:23:33 +0300

    Затемнение +правки

--- files/script/script.js
+++ files/script/script.js
@@ -1,6 +1,5 @@
 $(function(){
 
-
 	window.onbeforeunload = function () {
 	  window.scrollTo(0, 0);
 	}
@@ -15,18 +14,46 @@ $(function(){
 		})
 
 	$(document).ready(function(){
-		$('#hidden_2,#hidden_3,.fin_hid,.back_hid').css({'visibility':'hidden'})
+		$('#hidden_2,#hidden_3,.fin_hid,#hidden_1').css({'visibility':'hidden'})
 	})
-	$('#one').click(function(){$('.back_hid').css({'visibility':'visible'})});
-	$('#two').click(function(){$('#hidden_2').css({'visibility':'visible'})});
-	$('#three').click(function(){$('#hidden_3').css({'visibility':'visible'})});
-	$('#for_h_1').click(function(){$('.back_hid').css({'visibility':'hidden'})});
-	$('#for_h_2').click(function(){$('#hidden_2').css({'visibility':'hidden'})});
-	$('#for_h_3').click(function(){$('#hidden_3').css({'visibility':'hidden'})});
+	$('#one').click(function(){
+		$('#hidden_1').css({'visibility':'visible'})
+		$('main').css({'filter':'brightness(40%)'})
+		$('html, body').css({overflow: 'hidden'})
+	});
+	$('#two').click(function(){
+		$('#hidden_2').css({'visibility':'visible'})
+		$('main').css({'filter':'brightness(40%)'})
+		$('html, body').css({overflow: 'hidden'})
+	});
+	$('#three').click(function(){
+		$('#hidden_3').css({'visibility':'visible'})
+		$('main').css({'filter':'brightness(40%)'})
+		$('html, body').css({overflow: 'hidden'})
+	});
+	$('#for_h_1').click(function(){
+		$('#hidden_1').css({'visibility':'hidden'})
+		$('main').css({'filter':'brightness(100%)'})
+		$('html, body').css({overflow: 'auto'})
+	});
+	$('#for_h_2').click(function(){
+		$('#hidden_2').css({'visibility':'hidden'})
+		$('main').css({'filter':'brightness(100%)'})
+		$('html, body').css({overflow: 'auto'})
+	});
+	$('#for_h_3').click(function(){
+		$('#hidden_3').css({'visibility':'hidden'})
+		$('main').css({'filter':'brightness(100%)'})
+		$('html, body').css({overflow: 'auto'})
+	});
 
 	$('.butt').click(function(){
 		$('.fin_hid').css({'visibility':'visible'});
-		$('.back_hid,#hidden_2,#hidden_3').css({'visibility':'hidden'})});
-	$('#for_fh').click(function(){$('.fin_hid').css({'visibility':'hidden'})});
+		$('#hidden_1,#hidden_2,#hidden_3').css({'visibility':'hidden'})});
+	$('#for_fh').click(function(){
+		$('.fin_hid').css({'visibility':'hidden'})
+		$('main').css({'filter':'brightness(100%)'})
+		$('html, body').css({overflow: 'auto'})
+	});
 })
 
