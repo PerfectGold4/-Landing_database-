8eb74201529af6ad20f20e1be2053db38a24302b
rabotnik-da-da-ya <ilushavolkov@mail.ru>
2022-08-08 12:02:17 +0300

    Незначительные изменения
    Изменения в class и id, а также во всем внешнем виде кода

--- files/script/script.js
+++ files/script/script.js
@@ -1,59 +1,60 @@
 $(function(){
 /*при обновлении страница прокручивается вверх*/
 	window.onbeforeunload = function () {
-	  window.scrollTo(0, 0);
+	  window.scrollTo(0, 0)
 	}
 /*декоративное появление блока*/
-	$('p', '#fourth').fadeOut();
-	$('img', '#fourth').fadeOut();
+	$('p', '.anim_decor').fadeOut()
+	$('img', '.anim_decor').fadeOut()
 	$(window).scroll(function(){
 			if($(window).scrollTop()>350){
-				$('p', '#fourth').fadeIn(800);
-				$('img', '#fourth').fadeIn(800);
+				$('p', '.anim_decor').fadeIn(800)
+				$('img', '.anim_decor').fadeIn(800)
 			}
 		})
 /*оформление отправки эл. почты*/
-	$('.hid,.fin_hid,.error').css({'visibility':'hidden'});
+	$('.hid,.fin_hid,.error').css({'visibility':'hidden'})
 
-	$('#one').click(function(){
+	$('#prod_1').click(function(){
 		$('#hidden_1').css({'visibility':'visible'})
 		$('main').css({'filter':'brightness(40%)'})
 		$('html, body').css({overflow: 'hidden'})
-	});
-	$('#two').click(function(){
+	})
+	$('#prod_2').click(function(){
 		$('#hidden_2').css({'visibility':'visible'})
 		$('main').css({'filter':'brightness(40%)'})
 		$('html, body').css({overflow: 'hidden'})
-	});
-	$('#three').click(function(){
+	})
+	$('#prod_3').click(function(){
 		$('#hidden_3').css({'visibility':'visible'})
 		$('main').css({'filter':'brightness(40%)'})
 		$('html, body').css({overflow: 'hidden'})
-	});
+	})
+
 	$('#back_1').click(function(){
 		$('#hidden_1').css({'visibility':'hidden'})
 		$('#er_1').css({'visibility':'hidden'})
 		$('main').css({'filter':'brightness(100%)'})
 		$('html, body').css({overflow: 'auto'})
-	});
+	})
 	$('#back_2').click(function(){
 		$('#hidden_2').css({'visibility':'hidden'})
 		$('#er_2').css({'visibility':'hidden'})
 		$('main').css({'filter':'brightness(100%)'})
 		$('html, body').css({overflow: 'auto'})
-	});
+	})
 	$('#back_3').click(function(){
 		$('#hidden_3').css({'visibility':'hidden'})
 		$('#er_3').css({'visibility':'hidden'})
 		$('main').css({'filter':'brightness(100%)'})
 		$('html, body').css({overflow: 'auto'})
-	});
+	})
 
 	$('.back_fh').click(function(){
 		$('.fin_hid').css({'visibility':'hidden'})
 		$('main').css({'filter':'brightness(100%)'})
 		$('html, body').css({overflow: 'auto'})
-	});
+	})
 
 })
 
